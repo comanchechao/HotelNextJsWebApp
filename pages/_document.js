82c0350f85725917a56cbf65df028abbee2797f2
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-01-05 02:26:05 +0330

    Initial Commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import { createGetInitialProps } from "@mantine/next";
+const getInitialProps = createGetInitialProps();
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,79 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  plugins: [],
+  content: [
+    "./renderer/**/*.{vue,js,ts,jsx,tsx}",
+    "./pages/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        "[auto,auto,1fr]": "auto auto 1fr",
+      },
+      height: {
+        rem26: "26rem",
+        dialog: "32rem",
+        rem33: "33rem",
+
+        carousel: "38rem",
+      },
+      width: {
+        carousel: "50rem",
+      },
+      fontFamily: {
+        mainFont: ["LaleZar"],
+        SultanFont: ["SultanFont"],
+      },
+      fontSize: {
+        "10xl": "14rem",
+      },
+    },
+
+    screens: {
+      xs: { max: "767px" },
+      sm: { min: "768px", max: "991px" },
+      md: { min: "992px", max: "1199px" },
+      lg: { min: "1200px" },
+    },
+    colors: {
+      // Build your palette here
+      transparent: "transparent",
+      current: "currentColor",
+      mainPurple: "#6930c3",
+      mainBlue: "#00FFD1",
+      mainYellow: "#ffd500",
+      // mainBlue: "#1982C4",
+      mainWhite: "#dfc69c",
+      mainCream: "#FFE3CB",
+      // darkPurple: "#240046",
+      goldie: "#ff9e00",
+      white: colors.white,
+      black: colors.black,
+      gray: colors.neutral,
+      LightBlue: colors.sky,
+      red: colors.red,
+      blue: colors.blue,
+      yellow: colors.yellow,
+      blueGray: colors.slate,
+      Lime: colors.lime,
+      CoolGray: colors.gray,
+      Fuchsia: colors.fuchsia,
+      purple: colors.purple,
+      green: colors.green,
+      pink: colors.pink,
+      Rose: colors.rose,
+      Indigo: colors.indigo,
+      Amber: colors.amber,
+      Cyan: colors.cyan,
+      Emerald: colors.emerald,
+      Sky: colors.sky,
+    },
+  },
+
+  variants: {
+    extend: {},
+  },
+  // plugins: [require("daisyui")],
+};
