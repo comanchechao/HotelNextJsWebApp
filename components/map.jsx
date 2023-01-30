43b6fb8b4de295887b21a59edbafdaf07ebb56ec
Greez <imgreez@protonmail.com>
2023-01-30 08:01:54 +0330

    draggable map for admin

--- components/map.jsx
+++ components/map.jsx
@@ -3,8 +3,10 @@ import { TileLayer } from "react-leaflet/TileLayer";
 import { useMap } from "react-leaflet/hooks";
 import { Marker, DraggableMarker, render, Popup } from "react-leaflet";
 import { useState, useRef, useMemo, useCallback } from "react";
+import { Modal, Group } from "@mantine/core";
 
 export default function MapWithNoSSR() {
+  const [opened, setOpened] = useState(false);
   const center = {
     lat: 51.505,
     lng: -0.09,
@@ -47,18 +49,39 @@ export default function MapWithNoSSR() {
   }
 
   return (
-    <MapContainer
-      id="map"
-      className="w-96 h-96"
-      center={center}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <DraggableMarker />
-    </MapContainer>
+    <div>
+      <Modal
+        size="400px"
+        opened={opened}
+        onClose={() => setOpened(false)}
+        centered
+        title="Add new Hotel"
+      >
+        <MapContainer
+          id="map"
+          className="w-96 h-96"
+          center={center}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <DraggableMarker />
+        </MapContainer>
+      </Modal>
+
+      <Group position="center">
+        <button
+          onClick={() => {
+            setOpened(true);
+          }}
+          className="font-bold p-3 transition text-mainPurple hover:bg-darkPurple hover:text-gray-100 rounded-sm"
+        >
+          <p>انتخاب محل در نقشه</p>
+        </button>
+      </Group>
+    </div>
   );
 }
