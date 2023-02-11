62a779661efd35d5d6266857a4febb6fbd0c1aad
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-02-11 22:56:06 +0330

    Update profileInfo.jsx

--- pages/userProfile/profileInfo.jsx
+++ pages/userProfile/profileInfo.jsx
@@ -1,11 +1,14 @@
+import { TextInput } from "@mantine/core";
 import { Money, User } from "phosphor-react";
 import { useEffect, useState } from "react";
 import { supabase } from "../../lib/supabaseClient";
 
 export default function ProfileInfo() {
+  const [edit, setEdit] = useState(false);
+
   const [email, setEmail] = useState("");
   const [fullName, setFullName] = useState("");
-  const [id, setId] = useState("");
+  const [idCard, setIdCard] = useState("");
 
   const [phone, setPhone] = useState("");
   const [shaba, setShaba] = useState("");
@@ -24,7 +27,7 @@ export default function ProfileInfo() {
       setFullName(userData.data[0].fullName);
       setPhone(userData.data[0].phone);
       setCard(userData.data[0].card);
-      setId(userData.data[0].id);
+      setIdCard(userData.data[0].idCard);
       setShaba(userData.data[0].shaba);
 
       console.log(userData);
@@ -33,6 +36,21 @@ export default function ProfileInfo() {
     }
   }
 
+  async function editUser() {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (user) {
+      const updates = {
+        id: user.id,
+        email: user.email,
+      };
+      await supabase.from("profiles").upsert(updates);
+      console.log(email);
+      getSetUser();
+    } else {
+    }
+  }
   return (
     <div className="w-full h-full p-3 flex space-y-8 flex-col">
       <div className="w-full h-auto flex-col flex items-end justify-end bg-white drop-shadow-md rounded-lg">
@@ -41,30 +59,70 @@ export default function ProfileInfo() {
           <User className="mx-2 text-mainPurple" size={25} />
         </h2>
         <div className="w-full h-auto lg:h-36 border-t border-darkPurple border-dashed p-6 justify-center flex items-center">
-          <div className="flex flex-col justify-center items-center lg:items-end lg:justify-end space-y-7 my-3 text-right">
-            <div className="flex lg:flex-row flex-col items-center space-y-4 lg:space-y-0 lg:items-end justify-center lg:justify-end lg:space-x-4">
-              <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
-                {phone}
-              </h2>
-              <h2 className="text-lg text-gray-500">شماره تلفن</h2>
-              <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
-                {email}
-              </h2>
-              <h2 className="text-lg text-gray-500">ایمیل</h2>
+          {edit ? (
+            <div className="flex bg-goldie flex-col justify-center items-center lg:items-end lg:justify-end space-y-7 my-3 text-right">
+              <div className="flex  lg:flex-row flex-col items-center space-y-4 lg:space-y-0 lg:items-end justify-center lg:justify-end lg:space-x-4">
+                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
+                  {phone}
+                </h2>
+                <h2 className="text-lg text-gray-500">شماره تلفن</h2>
+                <TextInput
+                  className="text-2xl   text-right flex flex-col items-end "
+                  type="email"
+                  placeholder="ایمیل"
+                  label="ایمیل"
+                  size="md"
+                  withAsterisk
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <h2 className="text-lg text-gray-500">ایمیل</h2>
+              </div>
+              <div className="flex lg:flex-row flex-col items-center space-y-4 lg:space-y-0 lg:items-end justify-center lg:justify-end lg:space-x-4">
+                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
+                  {idCard}
+                </h2>
+                <h2 className="text-lg text-gray-500">کد ملی</h2>
+                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
+                  {fullName}{" "}
+                </h2>
+                <h2 className="text-lg text-gray-500">نام و نام خانوادگی</h2>
+              </div>
+              <button
+                onClick={editUser}
+                className="px-10 rounded-md transition ease-in duration-300 hover:bg-darkPurple border-r-8 border-mainBlue py-2 bg-mainPurple text-white text-sm m-3 self-start font-mainFont"
+              >
+                تایید
+              </button>
             </div>
-            <div className="flex lg:flex-row flex-col items-center space-y-4 lg:space-y-0 lg:items-end justify-center lg:justify-end lg:space-x-4">
-              <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
-                {id}
-              </h2>
-              <h2 className="text-lg text-gray-500">کد ملی</h2>
-              <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
-                {fullName}{" "}
-              </h2>
-              <h2 className="text-lg text-gray-500">نام و نام خانوادگی</h2>
+          ) : (
+            <div className="flex bg-goldie flex-col justify-center items-center lg:items-end lg:justify-end space-y-7 my-3 text-right">
+              <div className="flex  lg:flex-row flex-col items-center space-y-4 lg:space-y-0 lg:items-end justify-center lg:justify-end lg:space-x-4">
+                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
+                  {phone}
+                </h2>
+                <h2 className="text-lg text-gray-500">شماره تلفن</h2>
+                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
+                  {email}
+                </h2>
+                <h2 className="text-lg text-gray-500">ایمیل</h2>
+              </div>
+              <div className="flex lg:flex-row flex-col items-center space-y-4 lg:space-y-0 lg:items-end justify-center lg:justify-end lg:space-x-4">
+                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
+                  {idCard}
+                </h2>
+                <h2 className="text-lg text-gray-500">کد ملی</h2>
+                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
+                  {fullName}{" "}
+                </h2>
+                <h2 className="text-lg text-gray-500">نام و نام خانوادگی</h2>
+              </div>
             </div>
-          </div>
+          )}
         </div>
-        <button className="px-10 rounded-md transition ease-in duration-300 hover:bg-darkPurple border-r-8 border-mainBlue py-2 bg-mainPurple text-white text-sm m-3 self-start font-mainFont">
+        <button
+          onClick={() => setEdit(!edit)}
+          className="px-10 rounded-md transition ease-in duration-300 hover:bg-darkPurple border-r-8 border-mainBlue py-2 bg-mainPurple text-white text-sm m-3 self-start font-mainFont"
+        >
           ویرایش اطلاعات
         </button>
       </div>
