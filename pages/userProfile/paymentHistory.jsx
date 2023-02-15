73d4bfe558ba1cebaad1c9c5e277e2c848445095
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-02-15 12:03:13 +0330

    paymentHistory added

--- pages/userProfile/paymentHistory.jsx
+++ pages/userProfile/paymentHistory.jsx
@@ -1,3 +1,62 @@
+import { Table } from "@mantine/core";
+
 export default function PaymentHistory() {
-  return <div className="w-full h-full bg-mainBlue"></div>;
+  const elements = [
+    {
+      explanation: "lorem ipsum jeaad",
+      price: "22,550,000",
+      cardNum: "6315224588554488",
+      date: "2023/05/01",
+    },
+    {
+      explanation: "lorem ipsum jeaad",
+      price: "22,550,000",
+      cardNum: "6315224588554488",
+      date: "2023/05/01",
+    },
+    {
+      explanation: "lorem ipsum jeaad",
+      price: "22,550,000",
+      cardNum: "6315224588554488",
+      date: "2023/05/01",
+    },
+    {
+      explanation: "lorem ipsum jeaad",
+      price: "22,550,000",
+      cardNum: "6315224588554488",
+      date: "2023/05/01",
+    },
+    {
+      explanation: "lorem ipsum jeaad",
+      price: "22,550,000",
+      cardNum: "6315224588554488",
+      date: "2023/05/01",
+    },
+  ];
+  const rows = elements.map((element) => (
+    <tr key={element.date}>
+      <td>{element.explanation}</td>
+      <td>{element.cardNum}</td>
+      <td>{element.price}</td>
+      <td>{element.date}</td>
+    </tr>
+  ));
+  return (
+    <div className="w-full   h-carousel flex items-center justify-center flex-col space-y-6 bg-white px-14 py-10 text-center">
+      <h1>سوابق موجودی و تراکنش ها</h1>
+      <div className="h-full overflow-y-scroll w-full border border-mainPurple rounded-md flex items-start p-10 justify-center space-x-16">
+        <Table striped horizontalSpacing="xl" verticalSpacing="md">
+          <thead>
+            <tr>
+              <th>توضیحات</th>
+              <th>شماره کارت بانکی</th>
+              <th>مبلغ(ریال)</th>
+              <th>تاریخ و ساعت</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>{" "}
+        </Table>
+      </div>
+    </div>
+  );
 }
