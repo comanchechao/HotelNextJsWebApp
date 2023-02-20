bbfa3227706fa1c8c14c5afdbb0c01b9386eecf6
Greez <imgreez@protonmail.com>
2023-02-20 12:05:31 +0330

    fix on deploy

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     locales: ["fa", "tr"],
     defaultLocale: "fa",
+    localePath: path.resolve("./public/static/locales"),
   },
 };
