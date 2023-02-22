dcbf97673c3103d2588515f4e190bd0588a9fe5e
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-02-22 22:25:43 +0330

    Update profileInfo.jsx

--- pages/userProfile/profileInfo.jsx
+++ pages/userProfile/profileInfo.jsx
@@ -1,4 +1,4 @@
-import { TextInput } from "@mantine/core";
+import { TextInput, Loader } from "@mantine/core";
 import { Money, User } from "phosphor-react";
 import { useEffect, useState } from "react";
 import { supabase } from "../../lib/supabaseClient";
@@ -9,15 +9,16 @@ export default function ProfileInfo() {
   const [email, setEmail] = useState("");
   const [fullName, setFullName] = useState("");
   const [idCard, setIdCard] = useState("");
-
+  const [loading, setLoading] = useState(false);
   const [phone, setPhone] = useState("");
   const [shaba, setShaba] = useState("");
   const [card, setCard] = useState("");
 
   useEffect(() => {
     getSetUser();
-  });
+  }, []);
   async function getSetUser() {
+    setLoading(true);
     const {
       data: { user },
     } = await supabase.auth.getUser();
@@ -29,26 +30,29 @@ export default function ProfileInfo() {
       setCard(userData.data[0].card);
       setIdCard(userData.data[0].idCard);
       setShaba(userData.data[0].shaba);
-
-      console.log(userData);
+      setLoading(false);
     } else {
       console.log("Logged out");
     }
   }
 
   async function editUser() {
+    setLoading(true);
+
     const {
       data: { user },
     } = await supabase.auth.getUser();
+
     if (user) {
       const updates = {
         id: user.id,
-        email: user.email,
+        email: email,
       };
       await supabase.from("profiles").upsert(updates);
-      console.log(email);
-      getSetUser();
+      setLoading(false);
+      setEdit(true);
     } else {
+      console.log("User not found");
     }
   }
   return (
@@ -60,70 +64,90 @@ export default function ProfileInfo() {
         </h2>
         <div className="w-full h-auto lg:h-36 border-t border-darkPurple border-dashed p-6 justify-center flex items-center">
           {edit ? (
-            <div className="flex bg-goldie flex-col justify-center items-center lg:items-end lg:justify-end space-y-7 my-3 text-right">
-              <div className="flex  lg:flex-row flex-col items-center space-y-4 lg:space-y-0 lg:items-end justify-center lg:justify-end lg:space-x-4">
-                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
-                  {phone}
-                </h2>
-                <h2 className="text-lg text-gray-500">شماره تلفن</h2>
-                <TextInput
-                  className="text-2xl   text-right flex flex-col items-end "
-                  type="email"
-                  placeholder="ایمیل"
-                  label="ایمیل"
-                  size="md"
-                  withAsterisk
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <h2 className="text-lg text-gray-500">ایمیل</h2>
-              </div>
-              <div className="flex lg:flex-row flex-col items-center space-y-4 lg:space-y-0 lg:items-end justify-center lg:justify-end lg:space-x-4">
-                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
-                  {idCard}
-                </h2>
-                <h2 className="text-lg text-gray-500">کد ملی</h2>
-                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
-                  {fullName}{" "}
-                </h2>
-                <h2 className="text-lg text-gray-500">نام و نام خانوادگی</h2>
-              </div>
-              <button
-                onClick={editUser}
-                className="px-10 rounded-md transition ease-in duration-300 hover:bg-darkPurple border-r-8 border-mainBlue py-2 bg-mainPurple text-white text-sm m-3 self-start font-mainFont"
+            <div className="flex   flex-col justify-center items-center lg:items-end lg:justify-end space-y-7 my-3 text-right">
+              <form
+                className="flex h-full lg:flex-row flex-col items-center lg:space-y-0 space-y-4  lg:items-center justify-center lg:justify-between   lg:space-x-4"
+                onSubmit={editUser}
               >
-                تایید
-              </button>
+                {" "}
+                <div className="flex flex-col items-center justify-around space-y-2">
+                  <h2 className="text-sm text-gray-500">شماره تلفن</h2>
+                  <TextInput
+                    className="text-2xl   text-right flex flex-col items-end "
+                    type="number"
+                    placeholder="شماره تلفن"
+                    size="xs"
+                    withAsterisk
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                  <h2 className="text-sm text-gray-500">کد ملی</h2>
+                  <TextInput
+                    className="text-2xl   text-right flex flex-col items-end "
+                    type="text"
+                    placeholder="کد ملی"
+                    size="xs"
+                    withAsterisk
+                    onChange={(e) => setIdCard(e.target.value)}
+                  />
+                </div>
+                <div className="flex flex-col items-center justify-center space-y-2">
+                  <h2 className="text-sm text-gray-500">ایمیل</h2>
+                  <TextInput
+                    className="text-2xl   text-right flex flex-col items-end "
+                    type="email"
+                    placeholder="ایمیل"
+                    size="xs"
+                    withAsterisk
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <h2 className="text-sm text-gray-500">نام و نام خانوادگی</h2>
+                  <TextInput
+                    className="text-2xl   text-right flex flex-col items-end "
+                    type="text"
+                    placeholder="نام و نام خانوادگی"
+                    size="xs"
+                    withAsterisk
+                    onChange={(e) => setFullName(e.target.value)}
+                  />
+                </div>
+              </form>
             </div>
           ) : (
-            <div className="flex bg-goldie flex-col justify-center items-center lg:items-end lg:justify-end space-y-7 my-3 text-right">
+            <div className="flex   flex-col justify-center items-center lg:items-end lg:justify-end space-y-7 my-3 text-right">
               <div className="flex  lg:flex-row flex-col items-center space-y-4 lg:space-y-0 lg:items-end justify-center lg:justify-end lg:space-x-4">
-                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
+                <h2 className="text-md text-gray-800 border-b-2 border-mainPurple pb-1">
                   {phone}
                 </h2>
-                <h2 className="text-lg text-gray-500">شماره تلفن</h2>
-                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
+                <h2 className="text-md text-gray-500">شماره تلفن</h2>
+                <h2 className="text-md text-gray-800 border-b-2 border-mainPurple pb-1">
                   {email}
                 </h2>
-                <h2 className="text-lg text-gray-500">ایمیل</h2>
+                <h2 className="text-md text-gray-500">ایمیل</h2>
               </div>
               <div className="flex lg:flex-row flex-col items-center space-y-4 lg:space-y-0 lg:items-end justify-center lg:justify-end lg:space-x-4">
-                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
+                <h2 className="text-md text-gray-800 border-b-2 border-mainPurple pb-1">
                   {idCard}
                 </h2>
-                <h2 className="text-lg text-gray-500">کد ملی</h2>
-                <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
+                <h2 className="text-md text-gray-500">کد ملی</h2>
+                <h2 className="text-md text-gray-800 border-b-2 border-mainPurple pb-1">
                   {fullName}{" "}
                 </h2>
-                <h2 className="text-lg text-gray-500">نام و نام خانوادگی</h2>
+                <h2 className="text-md text-gray-500">نام و نام خانوادگی</h2>
               </div>
             </div>
           )}
         </div>
         <button
-          onClick={() => setEdit(!edit)}
+          onClick={() => setEdit(true)}
           className="px-10 rounded-md transition ease-in duration-300 hover:bg-darkPurple border-r-8 border-mainBlue py-2 bg-mainPurple text-white text-sm m-3 self-start font-mainFont"
         >
-          ویرایش اطلاعات
+          {loading ? (
+            <Loader size="sm" color="yellow" variant="bars" />
+          ) : (
+            <div>
+              {edit ? <p onClick={editUser}>تایید</p> : <p>ویرایش اطلاعات</p>}
+            </div>
+          )}
         </button>
       </div>
       <div className="w-full h-auto flex-col flex items-end justify-end bg-white drop-shadow-md rounded-lg">
@@ -134,14 +158,14 @@ export default function ProfileInfo() {
         <div className="w-full h-36 border-t border-darkPurple border-dashed p-6 justify-center flex items-center">
           <div className="flex flex-col justify-center items-center lg:items-end lg:justify-end space-y-7 my-3 text-right">
             <div className="flex lg:flex-row flex-col items-center justify-center lg:items-end lg:justify-end space-x-4">
-              <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
+              <h2 className="text-md text-gray-800 border-b-2 border-mainPurple pb-1">
                 {shaba}
               </h2>
-              <h2 className="text-lg text-gray-500">شماره شبا</h2>
-              <h2 className="text-lg text-gray-800 border-b-2 border-mainPurple pb-1">
+              <h2 className="text-md text-gray-500">شماره شبا</h2>
+              <h2 className="text-md text-gray-800 border-b-2 border-mainPurple pb-1">
                 {card}
               </h2>
-              <h2 className="text-lg text-gray-500">شماره حساب</h2>
+              <h2 className="text-md text-gray-500">شماره حساب</h2>
             </div>
           </div>
         </div>
