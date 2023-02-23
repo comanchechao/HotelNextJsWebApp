5cf62a855e48337072f0ee0f8cbb54f7207a68d7
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-02-23 08:15:23 +0330

    edit profile finished

--- pages/userProfile/profileInfo.jsx
+++ pages/userProfile/profileInfo.jsx
@@ -50,7 +50,7 @@ export default function ProfileInfo() {
       };
       await supabase.from("profiles").upsert(updates);
       setLoading(false);
-      setEdit(true);
+      setEdit(false);
     } else {
       console.log("User not found");
     }
@@ -137,7 +137,8 @@ export default function ProfileInfo() {
             </div>
           )}
         </div>
-        <button
+
+        {/* <button
           onClick={() => setEdit(true)}
           className="px-10 rounded-md transition ease-in duration-300 hover:bg-darkPurple border-r-8 border-mainBlue py-2 bg-mainPurple text-white text-sm m-3 self-start font-mainFont"
         >
@@ -148,31 +149,77 @@ export default function ProfileInfo() {
               {edit ? <p onClick={editUser}>تایید</p> : <p>ویرایش اطلاعات</p>}
             </div>
           )}
-        </button>
+        </button> */}
       </div>
       <div className="w-full h-auto flex-col flex items-end justify-end bg-white drop-shadow-md rounded-lg">
         <h2 className="flex items-center text-xl m-3 ">
           اطلاعات حساب بانکی
           <Money className="mx-2 text-mainPurple" size={25} />
         </h2>
-        <div className="w-full h-36 border-t border-darkPurple border-dashed p-6 justify-center flex items-center">
-          <div className="flex flex-col justify-center items-center lg:items-end lg:justify-end space-y-7 my-3 text-right">
-            <div className="flex lg:flex-row flex-col items-center justify-center lg:items-end lg:justify-end space-x-4">
-              <h2 className="text-md text-gray-800 border-b-2 border-mainPurple pb-1">
-                {shaba}
-              </h2>
-              <h2 className="text-md text-gray-500">شماره شبا</h2>
-              <h2 className="text-md text-gray-800 border-b-2 border-mainPurple pb-1">
-                {card}
-              </h2>
-              <h2 className="text-md text-gray-500">شماره حساب</h2>
+        {edit ? (
+          <div className="w-full h-36 border-t border-darkPurple border-dashed p-6 justify-center flex items-center">
+            <div className="flex   flex-col justify-center items-center lg:items-end lg:justify-end space-y-7 my-3 text-right">
+              <form
+                onSubmit={editUser}
+                className="flex lg:flex-row flex-col items-center justify-center lg:items-end lg:justify-end space-x-4"
+              >
+                <TextInput
+                  className="text-2xl   text-right flex flex-col items-end "
+                  type="text"
+                  placeholder="شماره شبا"
+                  size="xs"
+                  withAsterisk
+                  onChange={(e) => setShaba(e.target.value)}
+                />
+                <h2 className="text-md text-gray-500">شماره شبا</h2>
+                <TextInput
+                  className="text-2xl   text-right flex flex-col items-end "
+                  type="text"
+                  placeholder="شماره حساب"
+                  size="xs"
+                  withAsterisk
+                  onChange={(e) => setCard(e.target.value)}
+                />
+                <h2 className="text-md text-gray-500">شماره حساب</h2>
+              </form>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="w-full h-36 border-t border-darkPurple border-dashed p-6 justify-center flex items-center">
+            <div className="flex flex-col justify-center items-center lg:items-end lg:justify-end space-y-7 my-3 text-right">
+              <div className="flex lg:flex-row flex-col items-center justify-center lg:items-end lg:justify-end space-x-4">
+                <h2 className="text-md text-gray-800 border-b-2 border-mainPurple pb-1">
+                  {shaba}
+                </h2>
+                <h2 className="text-md text-gray-500">شماره شبا</h2>
+                <h2 className="text-md text-gray-800 border-b-2 border-mainPurple pb-1">
+                  {card}
+                </h2>
+                <h2 className="text-md text-gray-500">شماره حساب</h2>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      {loading ? (
         <button className="px-10 rounded-md transition ease-in duration-300 hover:bg-darkPurple border-r-8 border-mainBlue py-2 bg-mainPurple text-white text-sm m-3 self-start font-mainFont">
+          <Loader size="sm" color="yellow" variant="bars" />
+        </button>
+      ) : edit ? (
+        <button
+          onClick={editUser}
+          className="px-10 rounded-md transition ease-in duration-300 hover:bg-darkPurple border-r-8 border-mainBlue py-2 bg-mainPurple text-white text-sm m-3 self-start font-mainFont"
+        >
+          تایید
+        </button>
+      ) : (
+        <button
+          onClick={() => setEdit(true)}
+          className="px-10 rounded-md transition ease-in duration-300 hover:bg-darkPurple border-r-8 border-mainBlue py-2 bg-mainPurple text-white text-sm m-3 self-start font-mainFont"
+        >
           ویرایش اطلاعات
         </button>
-      </div>
+      )}
     </div>
   );
 }
