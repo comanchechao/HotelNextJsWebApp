7fc029bc870226aeb7df713443db5d72f603b629
Greez <imgreez@protonmail.com>
2023-02-28 13:37:10 +0330

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,20 +1,15 @@
-const locales = {
-  en: {
-    domain: "https://boutak.vercel.app/",
-  },
-  tr: {
-    domain: "https://boutak.vercel.app/tr",
-  },
-};
-
 module.exports = {
   i18n: {
-    locales: locales,
+    locales: ["fa", "tr"],
     defaultLocale: "fa",
     domains: [
       {
         domain: "https://boutak.vercel.app/",
         defaultLocale: "fa",
+      },
+      {
+        domain: "https://boutak.vercel.app/tr",
+        defaultLocale: "tr",
       },
     ],
   },
