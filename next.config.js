041f6bcc5705380a7bf4372f83902994d5c7513c
Greez <imgreez@protonmail.com>
2023-03-08 11:45:36 +0330

    security headers added

--- next.config.js
+++ next.config.js
@@ -4,5 +4,30 @@ const nextConfig = {
   reactStrictMode: true,
   i18n,
 };
+const { createSecureHeaders } = require("next-secure-headers");
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        headers: createSecureHeaders({
+          source: "/(.*)",
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: ["'self'", "boutak.com"],
+              scriptSrc: ["'self'", "'unsafe-inline'"],
+            },
+          },
+          xssProtection: true,
+          forceHTTPSRedirect: [
+            true,
+            { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true },
+          ],
+          referrerPolicy: "same-origin",
+        }),
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
--- pages/api/auth.js
+++
@@ -1,5 +0,0 @@
-import { supabase } from "../../lib/supabaseClient";
-
-export default function handler(req, res) {
-  supabase.auth.api.setAuthCookie(req, res);
-}
